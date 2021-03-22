e71533f6fc1a350d037ad97377dcf72bb501203d
ConnorGuard <35383137+ConnorGuard@users.noreply.github.com>
2021-03-23 09:26:56 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,74 @@
 import './css/App.css';
+import './animations.js';
 import React, { useEffect, useState } from 'react';
 const author = {
-  FirstName: 'Connor',
+  FirstName: 'Connor', 
   LastName: 'Guard'
 }
 function App() {
-  const[resourceType, setResourceType] = useState("Home");
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <body classsName = "body">
-        <Nav set = {setResourceType} />
-        <Page pageName = {resourceType}/>
+        <Nav />
+        <Page />
       </body>
     </div>
   );
 }
 
-function Nav(page){
-return(
+function Nav(){
+  const tNav = 900;
+  const[isToggleOn, setToggle] = useState((window.innerWidth>tNav)? true : false);
+  const[logoOn, toggleLogo] = useState((window.innerWidth>550)? true : false);
+  window.addEventListener('resize', ()=>{
+    console.log()
+    toggleLogo((window.innerWidth>550)? true: false);
+    setToggle((window.innerWidth>tNav)? true: false);
+  })
+  return(
   <div class = "nav">
+    <div class ="logo"> <a href = "#">CONNOR {(logoOn) ? <span href = "#">GUARD</span>: null} </a> </div> 
     <div class = "content"> 
-      <div class ="logo"> CONNOR GUARD </div> 
-      <button class = "button home bouncy" onClick={() => {page.set("Home"); window.location.href='#home';} }>HOME </button>
-      <button class = "button projects bouncy" onClick={() => {page.set("Projects"); window.location.href='#proj';}}>PROJECTS</button>
-      <button class = "button about bouncy" onClick={() => {page.set("About me"); window.location.href='#about';}}> ABOUT ME </button>
+      <a class = "toggle-button" onClick={() => {setToggle(!isToggleOn)}}>
+        <span class ="bar"></span>
+        <span class ="bar"></span>
+        <span class ="bar"></span>
+      </a>
+      {(isToggleOn) ? <Navbuttons /> : null}
     </div>
   </div>
 )
 }
 
+function Navbuttons(){
+    return(  
+    <nav class = "nav-buttons">
+      <button class = "button home bouncy" onClick={() => {window.location.href='#home';} }>HOME </button>
+      <button class = "button projects bouncy" onClick={() => {window.location.href='#proj';}}>PROJECTS</button>
+      <button class = "button about bouncy" onClick={() => {window.location.href='#about';}}>ABOUT</button>
+      <button class = "button contact bouncy" onClick={() => {window.location.href='#contact';}}> CONTACT </button>
+    </nav>)
+}
 function Page(props){
   return(
     <div class = "page">  
       <div id = "home" class = "page home"> 
-        <h2>{props.pageName}</h2> 
+        <div class = "greeting">
+          <h2>I am a Software Developer</h2>
+        </div>   
+          <img src = "images/noGlass.jpg"></img>
+         
       </div>   
       <div id = "proj" class = "page projects"> 
-        <h2>{props.pageName}</h2> 
+        <h2>Projects</h2> 
       </div>  
       <div id = "about" class = "page about"> 
-        <h2>{props.pageName}</h2> 
+        <h2>About</h2> 
+      </div>  
+      <div id = "contact" class = "page contact"> 
+        <h2>Contact</h2> 
       </div>  
     </div>
   )
