1896348fe02908491f4fc7f7b55f21324694f3bb
ConnorGuard <35383137+ConnorGuard@users.noreply.github.com>
2021-04-06 12:41:16 +1000

    update some styling

---
+++ src/Components/Nav.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+export default Nav;
+
+function Nav(){
+    const tNav = 700;
+    const tLogo = 900;
+    const[isToggleOn, setToggle] = useState((window.innerWidth>tNav)? true : false);
+    const[logoOn, toggleLogo] = useState((window.innerWidth>tLogo)? true : false);
+    window.addEventListener('resize', ()=>{
+      console.log()
+      toggleLogo((window.innerWidth>tLogo)? true: false);
+      setToggle((window.innerWidth>tNav)? true: false);
+    })
+    return(
+    <div class = "nav">
+      <div class ="logo"> <a href = "#home">CONNOR {(logoOn) ? <span href = "#">GUARD</span>: null} </a> </div> 
+      <div class = "content"> 
+        <a class = "toggle-button" onClick={() => {setToggle(!isToggleOn)}}>
+          <span class ="bar"></span>
+          <span class ="bar"></span>
+          <span class ="bar"></span>
+        </a>
+        {(isToggleOn) ? <Navbuttons /> : null}
+      </div>
+    </div>
+  )
+  }
+  
+  function Navbuttons(){
+      return(  
+      <nav class = "nav-buttons">
+        <button class = "button home bouncy" onClick={() => {window.location.href='#home';} }>HOME </button>
+        <button class = "button projects bouncy" onClick={() => {window.location.href='#proj';}}>PROJECTS</button>
+        <button class = "button about bouncy" onClick={() => {window.location.href='#about';}}>ABOUT</button>
+        <button class = "button contact bouncy" onClick={() => {window.location.href='#contact';}}> CONTACT </button>
+      </nav>)
+  }
